cbb99821f2bdddd744bd7c5a77a6a2694aa5299a
Adon Moskal <adonm@op.ac.nz>
2017-03-13 17:16:56 +1300

    starting to add angular calendar to homepage - not working yet

--- node/router/main.js
+++ node/router/main.js
@@ -1,9 +1,17 @@
 module.exports = function(app)
 {
-     app.get('/',function(req,res){
-        res.render('index.html')
-     });
-     app.get('/about',function(req,res){
-        res.render('about.html');
-    });
+	app.get('/', function (req, res){
+		console.log ('GET /');
+		res.render('main.ejs', {page:"index.ejs"});
+	});	
+	
+	app.get('/index', function (req, res){
+		console.log ('GET /index');
+		res.render('main.ejs', {page:"index.ejs"});
+	});
+	
+	app.get('/viewCustomers', function (req, res){
+		console.log ('GET /viewCustomers');
+		res.render('main.ejs', {page:"viewCustomers"});
+	});
 }
--- node/server.js
+++ node/server.js
@@ -8,34 +8,6 @@ app.set('views',__dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 	
-//Test serving static file
-app.get('/test', function (req, res){
-	console.log ('GET /test');
-	res.render('main.ejs', {page:"index.ejs"});
-});
-
-app.get('/index', function (req, res){
-	console.log ('GET /index');
-	res.render('main.ejs', {page:"index.ejs"});
-
-	});
-	
-app.get('/viewCustomers', function (req, res){
-	console.log ('GET /viewCustomers');
-	res.render('main.ejs', {page:"viewCustomers"});
-});
-
-app.get('/Zak', function (req, res){
-	console.log ('GET /Zak');
-	res.render('main.ejs', {page:"index"});
-});
-
-	
-app.get('/', function (req, res){
-	console.log ('GET /index');
-	res.render('index.html');
-	});
-	
 app.use(express.static(__dirname + '/public'));
 	
 server.listen (3000);
