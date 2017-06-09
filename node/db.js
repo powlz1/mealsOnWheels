13412b9b90d33e4c49549e437c8446397d2e64d2
sugrc1 <carlinsugrue@gmail.com>
2017-06-09 16:35:12 +1200

    please

--- node/db.js
+++ node/db.js
@@ -2,14 +2,9 @@ const Sequelize = require('sequelize');
 const mysql = require('mysql');
 
 //initialising javascript - http://docs.sequelizejs.com/en/v3/docs/getting-started/
-<<<<<<< HEAD
 
-var sequelize = new Sequelize('mow', 'root', '1234', { //change the second
-	host: 'localhost',
-=======
 var sequelize = new Sequelize('mow', 'root', '1234', { // MySQL login credentials, change the third string from "1234" to root
 	host: '127.0.0.1',
->>>>>>> 173a2df6c6946f1b059a6a08b6727060a0f77ab1
 	dialect: 'mysql',
 
 	pool: {
@@ -48,10 +43,7 @@ db.user.hasOne(db.driver);
 db.customer.belongsTo(db.user);
 db.driver.belongsTo(db.user);
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 173a2df6c6946f1b059a6a08b6727060a0f77ab1
 db.driver.belongsToMany(db.customer, {through: "customerDriver"});
 db.customer.belongsToMany(db.driver, {through: "customerDriver"});
 
